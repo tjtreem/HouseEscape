7d3dc284cbe3a03684d6abcf9662fad9e149571a
tjtreem <tjtreem@gmail.com>
2025-07-02 13:58:03 -0400

    Created uv virtual environment and began coding main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from game_data import rooms
+from player import Player
+
+def main():
+    player = Player(starting_room="basement")
+
+    print("Welcome to House Escape!")
+    print("Type 'help' for a list of commands.\n")
+
